38df09c74ffcf8d2fd38519d5f619d2b555860d5
Jonathan Gil <jhongilp@gmail.com>
2020-06-29 12:00:30 -0500

    Adding Result to handle errors

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # [RUST IN ACTION BOOK (Tim McNamara)](https://www.manning.com/books/rust-in-action)
 
-## Chapters
+## Chapters 3
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate rand;
+use rand::Rng;
+
+fn one_in(n: u32) -> bool {
+    rand::thread_rng().gen_weighted_bool(n)
+}
+
 #[derive(Debug)]
 struct File {
     name: String,
@@ -18,41 +25,50 @@ impl File {
         f
     }
 
-    fn read(f: &File, save_to:&mut Vec<u8>) -> usize {
-        let mut tmp = f.data.clone();
+    fn read(self: &File, save_to:&mut Vec<u8>) -> Result<usize, String> {
+        let mut tmp = self.data.clone();
         let read_length = tmp.len();
         save_to.reserve(read_length);
         save_to.append(&mut tmp); // tmp gets empty
         println!("tmp len: {}", read_length); // 5
         println!("tmp len2: {}", tmp.len()); // 0
     
-        read_length
+        Ok(read_length)
     }
 }
 
-fn open(f: &mut File) -> bool {
-    true
+fn open(f: File) -> Result<File, String> {
+    if one_in(10000) {
+        let err_msg = String::from("Permission denied");
+        return Err(err_msg);
+    }
+    Ok(f)
 }
 
-fn close(f: &mut File) -> bool {
-    true
+fn close(f: File) -> Result<File, String> {
+    if one_in(100000) {
+        let err_msg = String::from("Interrupted by signal!");
+        return Err(err_msg);
+    }
+    Ok(f)
 }
+
 
 fn main() {
     println!("Hello, world!");
 
-    let f3_data = vec![114, 117, 115, 116, 33];
-    let mut f3 = File::new_with_data("f3.txt", &f3_data);
+    let f_data = vec![114, 117, 115, 116, 33];
+    let mut f = File::new_with_data("f3.txt", &f_data);
 
     let mut buffer: Vec<u8> = vec![];
 
-    open(&mut f3);
-    let f3_length = File::read(&f3, &mut buffer);
-    close(&mut f3);
+    f = open(f).unwrap();
+    let f_length = f.read(&mut buffer).unwrap();
+    f = close(f).unwrap();
 
     let text = String::from_utf8_lossy(&buffer);
 
-    println!("{:?}", f3);
-    println!("{} is {} bytes long", &f3.name, f3_length);
+    println!("{:?}", f);
+    println!("{} is {} bytes long", &f.name, f_length);
     println!("text: {}", text);
 }
